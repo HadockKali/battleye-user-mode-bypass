b1c7a55c51c5839938d9a5e5f1f8aa3b365abe99
Navruc Dimitri Alexis <navruc.dimitri@gmail.com>
2020-03-14 03:12:53 +0200

    Added auto-build tools for non VS users ✔️
    Added auto-build .bat file to facilitate building the solution for machines who don't have VS 19/16 v242 installed.

---
+++ build.bat
@@ -0,0 +1,7 @@
+@echo off
+
+for /f "usebackq tokens=*" %%i in (`vswhere -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+  set InstallDir=%%i
+)
+
+"%InstallDir%\Common7\IDE\devenv.exe" /build release exploit.sln
